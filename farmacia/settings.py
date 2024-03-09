413f163342eb276e4779218fb0ffcca701d6d6fe
Mario Moreno <89218199+Mariomoreno4@users.noreply.github.com>
2024-03-09 01:10:42 -0700

    a

--- farmacia/settings.py
+++ farmacia/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT=BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
